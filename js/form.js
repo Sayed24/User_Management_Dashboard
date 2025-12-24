0f2e1e1aa7f18c29c8e45cd7cab316525bdb3367
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-25 00:37:14 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,1 +1,31 @@
+import { renderUsers } from "./ui.js";
+import { saveUsers } from "./storage.js";
+import { users } from "./state.js";
+
+const form = document.getElementById("userForm");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const newUser = {
+    id: Date.now().toString(),
+    firstName: firstName.value.trim(),
+    lastName: lastName.value.trim(),
+    email: email.value.trim(),
+    phone: phone.value.trim(),
+    city: city.value.trim(),
+    image: image.value.trim()
+  };
+
+  if (!newUser.firstName || !newUser.lastName || !newUser.email) {
+    alert("Please fill required fields");
+    return;
+  }
+
+  users.push(newUser);
+  saveUsers(users);
+  renderUsers(users);
+
+  form.reset();
+});
 
