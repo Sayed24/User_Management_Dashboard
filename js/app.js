ab46095ea7f2edc079692e9b152efb53ccbfa485
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-25 00:40:00 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+import { loadUsers } from "./storage.js";
+import { renderUsers } from "./ui.js";
+import "./form.js";
+import "./search.js";
+import "./theme.js";
+
+const users = loadUsers();
+renderUsers(users);
 
