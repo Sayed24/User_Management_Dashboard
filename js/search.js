d4fadc355a390051920b5f658649ee1828552e85
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-25 00:38:06 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,17 @@
+import { users } from "./state.js";
+import { renderUsers } from "./ui.js";
+
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+
+  const filtered = users.filter(user =>
+    `${user.firstName} ${user.lastName} ${user.email} ${user.city}`
+      .toLowerCase()
+      .includes(value)
+  );
+
+  renderUsers(filtered);
+});
 
