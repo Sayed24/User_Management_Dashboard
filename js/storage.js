e2c0b18138591d01ffd7f8fd5d8a88390c2695f5
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-25 00:34:42 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,15 @@
+import { setUsers } from "./state.js";
+
+const STORAGE_KEY = "users";
+
+export function loadUsers() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  const users = data ? JSON.parse(data) : [];
+  setUsers(users);
+  return users;
+}
+
+export function saveUsers(users) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(users));
+}
 
