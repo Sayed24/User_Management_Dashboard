23dbc298c0a6be16ed79ad9f82d4ad48b0952ed5
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-26 01:08:12 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,31 +1,47 @@
-import { renderUsers } from "./ui.js";
-import { saveUsers } from "./storage.js";
 import { users } from "./state.js";
+import { saveUsers } from "./storage.js";
+import { renderUsers } from "./ui.js";
 
-const form = document.getElementById("userForm");
+const preview = document.getElementById("previewImage");
 
-form.addEventListener("submit", e => {
+image.oninput = () => {
+  preview.src = image.value || "";
+};
+
+userForm.onsubmit = e => {
   e.preventDefault();
 
-  const newUser = {
-    id: Date.now().toString(),
-    firstName: firstName.value.trim(),
-    lastName: lastName.value.trim(),
-    email: email.value.trim(),
-    phone: phone.value.trim(),
-    city: city.value.trim(),
-    image: image.value.trim()
-  };
+  let valid = true;
 
-  if (!newUser.firstName || !newUser.lastName || !newUser.email) {
-    alert("Please fill required fields");
-    return;
-  }
+  if (!firstName.value) {
+    errFirstName.textContent = "First name required";
+    valid = false;
+  } else errFirstName.textContent = "";
 
-  users.push(newUser);
-  saveUsers(users);
-  renderUsers(users);
+  if (!lastName.value) {
+    errLastName.textContent = "Last name required";
+    valid = false;
+  } else errLastName.textContent = "";
 
-  form.reset();
-});
+  if (!email.value.includes("@")) {
+    errEmail.textContent = "Valid email required";
+    valid = false;
+  } else errEmail.textContent = "";
 
+  if (!valid) return;
+
+  users.push({
+    id: Date.now(),
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    phone: phone.value,
+    city: city.value,
+    image: image.value
+  });
+
+  saveUsers();
+  renderUsers();
+  userForm.reset();
+  preview.src = "";
+};
