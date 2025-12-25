0f1b23f21efa5525de13ed53b5fa6d178ed6416a
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-26 01:28:03 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,9 +1,7 @@
 import { users } from "./state.js";
 import { renderUsers } from "./ui.js";
 
-searchInput.oninput = () => {
-  const q = searchInput.value.toLowerCase();
-  renderUsers(users.filter(u =>
-    Object.values(u).join(" ").toLowerCase().includes(q)
-  ));
+searchInput.oninput=()=>{
+  const q=searchInput.value.toLowerCase();
+  renderUsers(users.filter(u=>Object.values(u).join("").toLowerCase().includes(q)));
 };
