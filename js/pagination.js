248b30cb1c4823b043b38e7a1ca74935f8cf78ba
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-26 01:25:15 +0300

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -1,1 +1,17 @@
+import { users, currentPage, PER_PAGE } from "./state.js";
+import { renderUsers } from "./ui.js";
 
+export function renderPagination() {
+  const total = Math.ceil(users.length / PER_PAGE);
+  pagination.innerHTML = "";
+
+  for (let i=1;i<=total;i++) {
+    const btn = document.createElement("button");
+    btn.textContent = i;
+    btn.onclick = () => {
+      window.currentPage = i;
+      renderUsers();
+    };
+    pagination.appendChild(btn);
+  }
+}
