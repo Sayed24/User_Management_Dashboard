3bdda31a2abd40cd752c4822690d5b87d87bb3f1
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-26 01:06:19 +0300

    Update demoUsers.js

--- js/demoUsers.js
+++ js/demoUsers.js
@@ -1,1 +1,20 @@
-
+export const demoUsers = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@example.com",
+    phone: "123456",
+    city: "New York",
+    image: "https://i.pravatar.cc/300?img=1"
+  },
+  {
+    id: 2,
+    firstName: "Sarah",
+    lastName: "Khan",
+    email: "sarah@example.com",
+    phone: "555888",
+    city: "London",
+    image: "https://i.pravatar.cc/300?img=2"
+  }
+];
