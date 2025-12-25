c176ad4a610edeb0e2c905f52cf5206ae6d27d5a
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-26 01:24:11 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,16 +1,12 @@
 import { users } from "./state.js";
 import { demoUsers } from "./demoUsers.js";
 
-const KEY = "users-dashboard";
+const KEY = "dashboard-users";
 
 export function loadUsers() {
   const data = localStorage.getItem(KEY);
-  if (data) {
-    users.push(...JSON.parse(data));
-  } else {
-    users.push(...demoUsers);
-    saveUsers();
-  }
+  if (data) users.push(...JSON.parse(data));
+  else { users.push(...demoUsers); saveUsers(); }
 }
 
 export function saveUsers() {
