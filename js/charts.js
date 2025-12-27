3d068d3f585ab687a3dfbb62a7aa0a6822c33a19
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-27 14:52:48 +0300

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,1 +1,24 @@
+import { users } from "./state.js";
 
+export function renderChart() {
+  const c = usersChart;
+  if (!c) return;
+
+  const ctx = c.getContext("2d");
+  ctx.clearRect(0,0,c.width,c.height);
+
+  const map = {};
+  users.forEach(u => map[u.city||"Other"] = (map[u.city||"Other"]||0)+1);
+
+  const keys = Object.keys(map);
+  const max = Math.max(...Object.values(map),1);
+  const w = c.width / keys.length;
+
+  keys.forEach((k,i)=>{
+    const h = (map[k]/max)*100;
+    ctx.fillStyle = "#2563eb";
+    ctx.fillRect(i*w,120-h,w-10,h);
+    ctx.fillStyle = "#000";
+    ctx.fillText(k,i*w+5,135);
+  });
+}
