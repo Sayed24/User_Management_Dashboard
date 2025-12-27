20fbd02959acbf9c87b953c9d92466c07daf5d42
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-28 00:11:16 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,18 +1,7 @@
-import { role } from "./state.js";
-
-loginBtn.onclick=()=>{
-  localStorage.setItem("role",roleSelect.value);
-  location.reload();
-};
-
-logoutBtn.onclick=()=>{
-  localStorage.removeItem("role");
-  location.reload();
-};
-
 export function initAuth(){
-  const r=localStorage.getItem("role");
-  if(!r) return;
-  loginSection.classList.add("hidden");
-  if(r==="admin") formSection.classList.remove("hidden");
+  loginBtn.onclick=()=>{
+    localStorage.setItem("userRole",loginRole.value);
+    loginScreen.classList.add("hidden");
+    app.classList.remove("hidden");
+  };
 }
