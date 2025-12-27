610f0fd5da74088ae335b1a420a59776b2ef6b74
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-27 14:50:41 +0300

    Update drag.js

--- js/drag.js
+++ js/drag.js
@@ -1,1 +1,20 @@
+import { users } from "./state.js";
+import { saveUsers } from "./storage.js";
+import { renderUsers } from "./ui.js";
 
+let dragged;
+
+export function enableDrag(container) {
+  [...container.children].forEach((card,i) => {
+    card.draggable = true;
+
+    card.ondragstart = () => dragged = i;
+    card.ondragover = e => e.preventDefault();
+    card.ondrop = () => {
+      const moved = users.splice(dragged,1)[0];
+      users.splice(i,0,moved);
+      saveUsers();
+      renderUsers();
+    };
+  });
+}
