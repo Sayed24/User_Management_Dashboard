ea840e6104610c9cd713d2968292639997827b2c
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-27 14:53:27 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 import { loadUsers } from "./storage.js";
 import { renderUsers } from "./ui.js";
-import { initAuth } from "./auth.js";
 import "./form.js";
 import "./search.js";
 import "./theme.js";
 
 loadUsers();
-initAuth();
 renderUsers();
 
-logoLink.onclick=e=>{
+logoLink.onclick = e => {
   e.preventDefault();
   window.scrollTo({top:0,behavior:"smooth"});
 };
