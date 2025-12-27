0312e9add51f62cde9e1925f51f7123898733342
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-28 00:26:42 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -2,21 +2,23 @@ import { users } from "./state.js";
 import { saveUsers } from "./storage.js";
 import { renderUsers } from "./ui.js";
 
-image.oninput=()=>previewImage.src=image.value;
+const form = document.getElementById("userForm");
 
-userForm.onsubmit=e=>{
+form.addEventListener("submit", e => {
   e.preventDefault();
-  users.push({
-    id:Date.now(),
-    firstName:firstName.value,
-    lastName:lastName.value,
-    email:email.value,
-    phone:phone.value,
-    city:city.value,
-    image:image.value||"https://i.pravatar.cc/300"
-  });
-  saveUsers();
+
+  const user = {
+    id: Date.now(),
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    phone: phone.value,
+    city: city.value,
+    image: image.value
+  };
+
+  users.push(user);
+  saveUsers(users);
   renderUsers();
-  userForm.reset();
-  previewImage.src="";
-};
+  form.reset();
+});
