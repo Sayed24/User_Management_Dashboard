f4bd20edd5cb2d4cf365b71fe6462a5f703d51d3
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-28 00:15:19 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 import { loadUsers } from "./storage.js";
 import { renderUsers } from "./ui.js";
+import { initAuth } from "./auth.js";
+import { initSidebar } from "./sidebar.js";
+import { initExport } from "./export.js";
 import "./form.js";
 import "./search.js";
 import "./theme.js";
+
+initAuth();
+initSidebar();
+initExport();
 
 loadUsers();
 renderUsers();
