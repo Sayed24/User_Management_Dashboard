55e8363302bfda55595746b2c3f847c8ecc32104
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-28 00:14:09 +0300

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,1 +1,15 @@
+import { users } from "./state.js";
 
+export function initExport(){
+  exportBtn.onclick=()=>{
+    const csv=[
+      Object.keys(users[0]).join(","),
+      ...users.map(u=>Object.values(u).join(","))
+    ].join("\n");
+
+    const a=document.createElement("a");
+    a.href=URL.createObjectURL(new Blob([csv]));
+    a.download="users.csv";
+    a.click();
+  };
+}
