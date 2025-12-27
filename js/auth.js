c67af4e8b636fd83993fe08b000d84d4ae43ae7e
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-28 00:28:17 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,21 @@
-export function initAuth(){
-  loginBtn.onclick=()=>{
-    localStorage.setItem("userRole",loginRole.value);
-    loginScreen.classList.add("hidden");
+export function initAuth() {
+  const login = document.getElementById("loginScreen");
+  const app = document.getElementById("app");
+
+  const saved = localStorage.getItem("user");
+  if (saved) {
+    login.classList.add("hidden");
     app.classList.remove("hidden");
+  }
+
+  loginBtn.onclick = () => {
+    localStorage.setItem("user", loginName.value);
+    login.classList.add("hidden");
+    app.classList.remove("hidden");
+  };
+
+  logoutBtn.onclick = () => {
+    localStorage.removeItem("user");
+    location.reload();
   };
 }
